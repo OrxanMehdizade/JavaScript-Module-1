c47ff762cc215a138214260851d4a4e48a01ebdd
Mehdi_ha01@itstep.edu.az <Mehdi_ha01@itstep.edu.az>
2023-10-01 22:46:10 +0400

    Initial commit

---
+++ JS/indexjs.js
@@ -0,0 +1,46 @@
+/*1  . Write a function that takes a string and displays statistics about
+it: number of letters, number of digits, and number of other
+characters. */
+
+/*2  Write a function that takes a two-digit number and returns it
+in text form.
+For example: 35 – thirty five, 89 – eighty nine,
+12 – twelve.  */
+
+/*3  Write a function that replaces capital letters with small,
+small with capital, and numbers with underscore in the
+received string.  */
+
+/*4  Write a function that converts names of css styles with a hyphen
+to a name in the CamelCase style: font-size to fontSize,
+background-color to backgroundColor, text-align to
+textAlign. */
+
+/*5   Write a function that takes a phrase and converts it to an abbreviation.
+For example: cascading style sheets to CSS, object-oriented
+programming to OOP.*/
+
+/*6  Write a function that takes any number of strings, combines
+them into one long string and returns it*/
+
+/*7 Write a calculator function. The function takes a string with
+an example, determines what is to be done (+ – * /), converts
+operands to numbers, solves it, and returns the result.  */
+
+/*8  rite a function that gets a url and displays detailed information about it.*/
+
+/*9 Write a function that takes a string and delimiter and returns
+an array of substrings that are broken up using the specified
+delimiter.
+For example: line “10/08/2020”, separator “/”, the result: “10”,
+“08”, “2020”.
+While performing the task, do not use the split() function. */
+
+/*10  Write a function to output text according to a given template.
+As the first parameter, the function takes a template that can
+use % in the text, index of the input parameter is specified after
+the % sign. When outputting index instead of %, output value
+of the corresponding input parameter.
+For example: print(“Today is %1 %2.%3.%4”, “Monday”,
+10, 8, 2020) must print “Today is Monday 10.8.2020”*/
+
